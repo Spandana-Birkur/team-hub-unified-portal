978c569500e5ff23cffd4c2f2fa5dccac9c09479
DaKing425 <vivekreddysama@gmail.com>
2025-08-07 14:05:34 -0700

    rand

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  optimizeDeps: {
+    include: ['react-dom/client', 'react-router-dom'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
